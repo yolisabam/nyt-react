dc16120964b2a46298f126d410270e7ce4dbd5f3
Yolisa Bam <yolisabam@calvisitor-10-105-171-180.calvisitor.1918.berkeley.edu>
2017-11-20 15:01:59 -0800

    progress on front end

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const articleRoutes = require("./articles-api")
 
-router.use("/", articleRoutes);
+router.use("/articles", articleRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
+const apiRoutes = require("./api");
 
-const apiRoutes = require("/api", apiRoutes);
+router.use("/api", apiRoutes);
+
+
 
 router.use(function(req,res){
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
